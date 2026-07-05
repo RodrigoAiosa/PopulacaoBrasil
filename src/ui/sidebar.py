4d4cf5f31debb5d27ad1ec596d2fde89dcb17e8d
Rodrigo Aiosa <32776696+RodrigoAiosa@users.noreply.github.com>
2026-07-04 21:52:33 -0300

    Update sidebar.py

--- src/ui/sidebar.py
+++ src/ui/sidebar.py
@@ -278,7 +278,7 @@ class SidebarFilters:
                         font-weight: 600;
                         letter-spacing: 0.3px;
                     ">
-                        Rodrigo Aiôsa
+                        Rodrigo Aiosa
                     </div>
                     <div style="
                         color: #EAF2EE;
